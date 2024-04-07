e34dd7e1079249e9b8c48224728a4723a58ace51
dannysyed <dannysyed20@gmail.com>
2024-04-07 08:29:24 -0400

    Adding of data and model and images

--- lib/models.dart
+++ lib/models.dart
@@ -1,15 +1,28 @@
 class Candidate {
-  String name;
-  String position;
-  String imageUrl;
+  final String name;
+  final String position;
+  final String imageUrl;
+
   Candidate(
       {required this.name, required this.position, required this.imageUrl});
 }
 
+class Post {
+  final Candidate candidate;
+  final String message;
+
+  Post({required this.candidate, required this.message});
+}
+
 class JobListing {
-  String title;
-  String company;
-  String logoUrl;
+  final String title;
+  final String company;
+  final String logoUrl;
+  final String description;
+
   JobListing(
-      {required this.title, required this.company, required this.logoUrl});
+      {required this.title,
+      required this.company,
+      required this.logoUrl,
+      required this.description});
 }
